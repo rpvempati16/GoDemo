a6feda784a5b0cb688cf911480905a7550ff9a9a
Rajendra Vempati <vempati.rajendra@hotmail.com>
2024-08-31 09:22:22 -0400

    First Demo project on Go Lang

---
+++ go-microservice/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
